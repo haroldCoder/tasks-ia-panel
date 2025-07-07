ffb4bd8a866b8ec1dd54199b383aa3f72feb2b69
haroldCoder <69125414+haroldCoder@users.noreply.github.com>
2025-07-07 18:31:40 +0000

    hotfix: add clerkProvider to all views

--- app/tasks/[id]/[id_aditional]/page.tsx
+++ app/tasks/[id]/[id_aditional]/page.tsx
@@ -5,19 +5,22 @@ import Loading from "./loading";
 import { AditionalTask } from "@/app/layouts/AditionalTask/AditionalTask";
 import { Provider } from "react-redux";
 import { store } from "@/app/store/store";
+import { ClerkProvider } from "@clerk/nextjs"
 
 const page = ({
   params,
 }: {
   params: Promise<{ id: number; id_aditional?: number }>;
 }) => {
-  const {id, id_aditional} = React.use(params);
+  const { id, id_aditional } = React.use(params);
 
   return (
     <Suspense fallback={<Loading />}>
-      <Provider store={store}>
-        <AditionalTask id={id} id_aditional={id_aditional} />
-      </Provider>
+      <ClerkProvider>
+        <Provider store={store}>
+          <AditionalTask id={id} id_aditional={id_aditional} />
+        </Provider>
+      </ClerkProvider>
     </Suspense>
   );
 };
